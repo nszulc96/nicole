0ce1e9aef809d28373ad9d8d62a2a22d5b388fa6
Nicole Szulc <nszulc96@outlook.com>
2024-12-01 20:13:06 -0300

    removing imgs

--- script.js
+++ script.js
@@ -102,17 +102,6 @@ $(document).ready(function () {
                 <p>
                     A library management project developed in C#. This system allows users to catalogue, search and track book loans in an intuitive way.
                 </p>
-                <div class="project-images">
-                    <a href="./bibliomania/splash-screen.png" target="_blank">
-                        <img src="./bibliomania/splash-screen.png" alt="Screen Biblio Mania" class="imgsBiblio">
-                    </a>
-                    <a href="./bibliomania/login-screen.png" target="_blank">
-                        <img src="./bibliomania/login-screen.png" alt="Screen Biblio Mania" class="imgsBiblio">
-                    </a>
-                    <a href="./bibliomania/rent.png" target="_blank">
-                        <img src="./bibliomania/rent.png" alt="Screen Biblio Mania" class="imgsBiblio">
-                    </a>
-                </div>
             </div>
         `,
         social: `
